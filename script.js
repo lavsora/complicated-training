ddeb252ab12fc7aa603dc3f0318f92bcd7b562d1
lavsora <ya.karpich@gmail.com>
2022-06-03 22:41:11 +0300

    week days

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+const day = document.getElementById('days');
+const todayDay = new Date();
+
+const days = function() {
+    week.forEach(function(item, i) {
+        let newDiv = document.createElement('div');
+        if (i === +todayDay.getDay()-1) {
+          newDiv.classList.add('today');
+          newDiv.textContent = week[i]
+        }
+        if (item == 'Суббота' || item == 'Воскресенье') { 
+          newDiv.classList.add('italic');
+          newDiv.textContent = week[i];
+        } else {
+          newDiv.textContent = week[i]
+        }
+
+        day.appendChild(newDiv);
+    })
+}
+
+days()
