4aea770e54253efb4c4eb745cb96c8935da32e03
lavsora <ya.karpich@gmail.com>
2022-06-12 15:11:19 +0300

    fix bag for sunday

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 const day = document.getElementById('days');
-const todayDay = new Date();
+const todayDay = (6 + new Date().getDay()) % 7;
 
 const days = function() {
     week.forEach(function(item, i) {
         let newDiv = document.createElement('div');
-        if (i === +todayDay.getDay() -1) {
+        if (i === todayDay) {
           newDiv.classList.add('today');
           newDiv.textContent = week[i]
         }
