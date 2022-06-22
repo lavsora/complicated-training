9904b90c8509dbdc74aa1926be2144dd41e00a67
lavsora <ya.karpich@gmail.com>
2022-06-22 14:14:54 +0300

    refactor code

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 'use strict'
 
-let hiSpan = document.getElementById('hi')
-let daySpan = document.getElementById('day')
-let timeSpan = document.getElementById('time')
-let newYearDaysSpan = document.getElementById('newYearDays')
-const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+const hiSpan = document.getElementById('hi');
+const daySpan = document.getElementById('day');
+const timeSpan = document.getElementById('time');
+const newYearDaysSpan = document.getElementById('newYearDays');
+const arrDay = ['день', 'дня', 'дней'];
 
-const declinationWord = (date, words) => {
-    const cases = [2, 0, 1, 1, 1, 2];
-    return words[(date % 100 > 4 && date % 100 < 20) ? 2 : cases[(date % 10 < 5) ? date % 10 : 5]];
-}
+let newYear = String(+new Date().toLocaleDateString('ru', { year: 'numeric' }) + 1);
 
 const setHi = (time) => {
     if (12 > time && time >= 4) {
@@ -23,30 +20,37 @@ const setHi = (time) => {
     }
 }
 
+const declinationWord = (date, words) => {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return words[(date % 100 > 4 && date % 100 < 20) ? 2 : cases[(date % 10 < 5) ? date % 10 : 5]];
+}
+
 const getTimeRemaining = (newYear) => {
-    let arrDay = ['день', 'дня', 'дней'];
     let date = new Date()
 
     let hiTime = date.getHours()
-    let dayWeek = week[date.getDay()]
+    let dayWeek = date.toLocaleDateString('ru', { weekday: 'long' })[0].toUpperCase() + date.toLocaleDateString('ru', { weekday: 'long' }).slice(1)
     let timeClock = date.toLocaleTimeString('en')
 
     let dateStop = new Date(newYear).getTime();
-    let dateNow = new Date().getTime();
+    let dateNow = date.getTime();
     let timeRemaining = (dateStop - dateNow) / 1000;
+    
     let daysLeft = Math.floor(timeRemaining / 60 / 60 / 24);
 
     return { timeRemaining, dayWeek, timeClock, daysLeft, arrDay, hiTime }
 }
 
 const showTime = () => {
-    let getTime = getTimeRemaining('1 january 2023')
+    let getTime = getTimeRemaining(newYear);
 
     hiSpan.textContent = setHi(getTime.hiTime)
     daySpan.textContent = getTime.dayWeek
     timeSpan.textContent = getTime.timeClock
     newYearDaysSpan.textContent = `До нового года осталось ${getTime.daysLeft} ${declinationWord(getTime.daysLeft, getTime.arrDay)} `
 }
+
+showTime();
 
 setInterval(showTime, 1000)
 
